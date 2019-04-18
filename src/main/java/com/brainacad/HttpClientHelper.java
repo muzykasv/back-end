487d90e6b7b0be42d83f3f21db5ec3c395b03721
Sergey <muzykasv@ukr.net>
2019-04-18 21:43:34 +0300

    Get and Post

--- src/main/java/com/brainacad/HttpClientHelper.java
+++ src/main/java/com/brainacad/HttpClientHelper.java
@@ -10,15 +10,19 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.apache.http.protocol.HTTP.USER_AGENT;
 
 public class HttpClientHelper {
 
-    public static HttpResponse get(String endpointUrl, String parameters){
-       //TODO: написать метод для GET запроса с хедерами по умолчанию
-       return null;
+    public static HttpResponse get(String endpointUrl, String parameters) throws IOException {
+        //Создаём переменую headers типа Map
+        Map<String, String> headers=new HashMap<>();
+        //Добавляем в headers наш заголовок
+        headers.put("User-Agent", "My-Test-User-Agent");
+             return get(endpointUrl, parameters, headers);
     }
 
     //REST GET запрос
@@ -41,9 +45,13 @@ public class HttpClientHelper {
     }
 
 
-    public static HttpResponse post(String endpointUrl, String parameters){
-        //TODO: написать метод для POST запроса с хедерами по умолчанию
-        return null;
+    public static HttpResponse post(String endpointUrl, String parameters) throws IOException {
+
+        //Создаём переменую headers типа Map
+        Map<String, String> headers=new HashMap<>();
+        //Добавляем в headers наш заголовок
+        headers.put("User-Agent", "My-Test-User-Agent");
+              return post(endpointUrl, parameters, headers);
     }
 
     public static HttpResponse post(String endpointUrl, String body, Map<String, String> headers) throws IOException{
